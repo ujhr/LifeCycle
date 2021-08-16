54e0b0261af7111c84acde6d2360fff7723afd90
MasayaUjihara <muappscontact@gmail.com>
2021-08-16 23:57:01 +0900

    Initial Commit

---
+++ LifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by 宇治原真也 on 2021/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
